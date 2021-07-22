a0af8a36bd8f1b5669c55f326a23428da78f00f4
Rachita Saha <rachita19082@iiitd.ac.in>
2021-07-22 13:40:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,21 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Maze from './Maze';
 
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-const element = <Maze></Maze>
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Maze />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
